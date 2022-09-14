50ea8bd3829fbdec1bc14535a9bcaed98fa17736
Maximilian Algehed <m.algehed@gmail.com>
2022-09-14 15:13:03 +0200

    Initial monad

--- utxo-model/src/UTxOModel.hs
+++ utxo-model/src/UTxOModel.hs
@@ -5,3 +5,14 @@ import Value
 data UTxO a = NoUTxO
             | UTxO Value a
             deriving (Ord, Eq, Show)
+
+newtype UTxORef a = UTxORef { unUTxORef :: Integer }
+
+data SmartContract a where
+  Done       :: a -> SmartContract a
+  Atomically :: (UTxO a -> UTxO b)
+             -> UTxORef a
+             -> (UTxORef b -> SmartContract c)
+             -> SmartContract c
+
+deriving instance Functor SmartContract

8f4e120b8b21ba972d9c0794ffdb1f9e14a7367d
Maximilian Algehed <m.algehed@gmail.com>
2022-09-21 15:47:17 +0200

    future is bright and full of candy canes

--- utxo-model/src/LinearUTxOModel.hs
+++ utxo-model/src/LinearUTxOModel.hs
@@ -82,72 +82,14 @@ castUTxO (UTxO (AnyOwner addr) addr' value datum)
   | addr == addr' = mkUTxO addr' value datum
   | otherwise     = failTx () "castUTxO failed."
 
--- minAda :: UTxO a -> Integer
--- minAda _ = 2_000_000
-
 -- TODO:
--- * Figure out SmartContrat monad to force evaluation of "validators"
--- * Figure out module system stuff to force you to actually use the "validators"
--- * Signature PubKeyOwner can only come from the monad in some listigt sätt
+-- This is the overall idea of how to build transactions and *true* script contexts:
 --
-{- The linear idea:
-
-  We want to write "builder functions" that work on UTxOs to define contracts.
-  However, we don't have a whole compiler back-end to use to turn these builder
-  functions into actual validators. This is a problem because in order to emulate
-  the chain we need to guarantee that our "pure functions transforming UTxO sets"
-  (i.e. our view of transactions) are actually in comformance with the validators
-  *implied by our builders*.
-
-  Barring some magic the only real way to do this is to ensure that the only way
-  to actually transform the UTxOs belonging to a given contract is to *use the
-  actual builder functions* to build transactions that are value-preserving.
-
-  Now, one could imagine that the way to do this is to only expose the builder
-  functions as the destructors of "auction" or "escrow" UTxOs (using some
-  type-index + module system magic and not allowing you to pattern-match on
-  UTxOs). However, *this is not enough* because one might write a function like:
-
-  cheat auctionUTxO = UTxO myWallet (valueOf auctionUTxO) ()
-
-  that would be able to "steal" the money from the auction contract.
-  You might think that you could avoid this by not exposing the `valueOf` function
-  (which anyway isn't a workable solution!). But you still have the same problem!
-
-  cheat auctionUTxO = UTxO myWallet (ada 10) ()
-
-  This function would steal the value of the auction contract if and
-  only if it happens to be 10 ada.
-
-  The way around this is to use linear types to *guarantee* that any
-  transaction (pure function from utxos to utxos) actually consumes
-  the input transactions using the exposed builder functions!
-
-  If we force transactions to be of a type like:
-
-  cheat :: UTxO "auction" AuctionData -o UTxO SomeWallet ()
-
-  we don't have this problem any more!
-
-  Now, there are lots of details to work out but that's later Ulf and Max's problem.
-  Now it's playtime.
-
-  One more thing!
-
-  To use a `UTxO SomeWallet ()` one would need something like a function of type
-  `UTxO SomeWallet () -o ()` that would acutally consume the UTxO (you can't pattern
-  match on UTxOs remember!). Now, no such function should exist in general but you could
-  have a monadic action in the `SmartContract` monad that gives you this - something like:
-
-  signedBy wallet1 $ \ destructor :: UTxO SomeWallet () -o () -> myTxCode destructor
-
-  where `destructor` here would check that the input UTxO actually belongs to Wallet 1.
-
-  ---
-
-  One of many questions that remains to be answered is what the implications of this are
-  when we go to our own stand-alone language? Clearly (?) we still need some form of linear
-  type system - maybe it can be cleverererrere than Haskell's? Anyway, now it's *really*
-  play time!
-
--}
+-- submit :: TxRepType -> SmartContract () (or whatever)
+--
+-- withSignature :: PubKeyHash -> (Signature PubKeyOwner -> TxRepType) -> TxRepTyp
+-- withTime :: (TrueTime -> TxRepType) -> TxRepType
+-- transform :: (UTxOs %1 -> UTxOs) -> TxRepType
+--
+-- The monad can insert the correct time and check that we are currently running on the
+-- wallet that can sign for a given pub key hash etc.

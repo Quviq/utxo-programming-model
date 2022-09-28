19bd9dc63d112aa6ac8a04323ca1947342ec2ec6
Maximilian Algehed <m.algehed@gmail.com>
2022-09-28 09:43:03 +0200

    thoughts

--- utxo-model/src/LinearUTxOModel.hs
+++ utxo-model/src/LinearUTxOModel.hs
@@ -129,10 +129,13 @@ newtype UTxORef owner datum = UTxORef { getRef :: Int }
 type UTxORefs = TList2 UTxORef
 
 data TxRep inputs outputs where
+  -- TODO: this should probably be `UTxOs inputs %1 -> MaybeUTxOs outputs`? So that in
+  -- some cases we can omit specific UTxOs in the output.
   Transform     :: (UTxOs inputs %1 -> UTxOs outputs) %1 -> TxRep inputs outputs
   WithSignature :: PubKeyHash -> (Signature PubKeyOwner -> TxRep inputs outputs) -> TxRep inputs outputs
   WithTime      :: Time -> Time -> (TrueTime -> TxRep inputs outputs) -> TxRep inputs outputs
 
 data SmartContract a where
   Done   :: a -> SmartContract a
+  -- TODO: this should be MaybeUTxORefs in the output?
   Submit :: TxRep inputs outputs -> UTxORefs inputs -> (UTxORefs outputs -> SmartContract a) -> SmartContract a
